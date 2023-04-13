4a6ed4c3d5e15710bf321c5c04d44f4e5cc2a132
NikolaiMoskito <nikolailehbrink@outlook.com>
2023-04-13 16:45:51 +0200

    Update: Alt-Text synchronization logic to replace existing with the Media Library Alt text
    - Updated the code to synchronize Alt text from the Media Library even when an Alt text is already present in the block
    - Improved compatibility with the block editor and overall user experience
    - Ensured that the latest Alt text from the Media Library is used for better accessibility and SEO

--- synchronized-alt-text.js
+++ synchronized-alt-text.js
@@ -10,33 +10,25 @@ wp.domReady(function () {
     }
   );
 
-  function updateBlockAltText(block) {
-    var imgId = block.attributes.id;
-    var currentAlt = block.attributes.alt;
-    if (imgId && !currentAlt) {
-      var media = wp.data.select("core").getMedia(imgId);
-      if (media && media.alt_text) {
-        wp.data
-          .dispatch("core/block-editor")
-          .updateBlockAttributes(block.clientId, { alt: media.alt_text });
-      }
-    }
-  }
-
   wp.data.subscribe(function () {
     var selectedBlock = wp.data.select("core/block-editor").getSelectedBlock();
     if (selectedBlock && selectedBlock.name === "core/image") {
-      updateBlockAltText(selectedBlock);
-    }
-  });
+      var imgId = selectedBlock.attributes.id;
+      var currentAlt = selectedBlock.attributes.alt;
 
-  wp.hooks.addAction(
-    "editor.BlockListBlock.onClick",
-    "my-plugin/synchronized-alt-text",
-    function (block) {
-      if (block && block.name === "core/image") {
-        updateBlockAltText(block);
+      if (imgId) {
+        var media = wp.data.select("core").getMedia(imgId);
+        if (media && media.alt_text) {
+          // Check if the current Alt text is different from the media Alt text
+          if (currentAlt !== media.alt_text) {
+            wp.data
+              .dispatch("core/block-editor")
+              .updateBlockAttributes(selectedBlock.clientId, {
+                alt: media.alt_text,
+              });
+          }
+        }
       }
     }
-  );
+  });
 });
