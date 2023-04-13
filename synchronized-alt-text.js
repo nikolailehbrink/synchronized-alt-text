ed92c40e0269e47cd04b1c62b71d1d612fb43c57
NikolaiMoskito <nikolailehbrink@outlook.com>
2023-04-13 16:02:05 +0200

    Update synchronized-alt-text.js
    - Add fragment with link to sync the alt text on click

--- synchronized-alt-text.js
+++ synchronized-alt-text.js
@@ -1,42 +1,76 @@
 wp.domReady(function () {
-  wp.hooks.addFilter(
-    "blocks.registerBlockType",
-    "my-plugin/synchronized-alt-text",
-    function (settings, name) {
-      if (name === "core/image") {
-        settings.attributes.alt.default = "";
-      }
-      return settings;
-    }
-  );
-
-  function updateBlockAltText(block) {
-    var imgId = block.attributes.id;
-    var currentAlt = block.attributes.alt;
-    if (imgId && !currentAlt) {
-      var media = wp.data.select("core").getMedia(imgId);
-      if (media && media.alt_text) {
-        wp.data
-          .dispatch("core/block-editor")
-          .updateBlockAttributes(block.clientId, { alt: media.alt_text });
-      }
+  function updateBlockAltText(clientId, media) {
+    if (media && media.alt_text) {
+      wp.data
+        .dispatch("core/block-editor")
+        .updateBlockAttributes(clientId, { alt: media.alt_text });
     }
   }
 
-  wp.data.subscribe(function () {
-    var selectedBlock = wp.data.select("core/block-editor").getSelectedBlock();
-    if (selectedBlock && selectedBlock.name === "core/image") {
-      updateBlockAltText(selectedBlock);
-    }
-  });
-
-  wp.hooks.addAction(
-    "editor.BlockListBlock.onClick",
+  wp.hooks.addFilter(
+    "editor.BlockEdit",
     "my-plugin/synchronized-alt-text",
-    function (block) {
-      if (block && block.name === "core/image") {
-        updateBlockAltText(block);
-      }
+    function (BlockEdit) {
+      return function (props) {
+        var blockName = props.name;
+        var attributes = props.attributes;
+
+        if (blockName === "core/image") {
+          var media = wp.data.select("core").getMedia(attributes.id);
+
+          // Synchronize empty alt text automatically.
+          if (!attributes.alt) {
+            updateBlockAltText(props.clientId, media);
+          }
+
+          var syncAltTextLink = attributes.alt
+            ? wp.element.createElement(
+                "a",
+                {
+                  href: "#",
+                  onClick: function (event) {
+                    event.preventDefault();
+                    media = wp.data.select("core").getMedia(attributes.id); // Fetch the latest media object
+                    updateBlockAltText(props.clientId, media);
+                  },
+                  style: {
+                    display: "block",
+                    marginTop: "5px",
+                    cursor: "pointer",
+                    textDecoration: "underline",
+                  },
+                },
+                "Synchronize Alt Text"
+              )
+            : null;
+
+          var withInspectorControls = wp.compose.createHigherOrderComponent(
+            function (BlockEdit) {
+              return function (props) {
+                return wp.element.createElement(
+                  wp.element.Fragment,
+                  {},
+                  wp.element.createElement(BlockEdit, props),
+                  wp.element.createElement(
+                    wp.editor.InspectorControls,
+                    {},
+                    wp.element.createElement(
+                      wp.components.PanelBody,
+                      { title: "Alt Text Synchronization" },
+                      syncAltTextLink
+                    )
+                  )
+                );
+              };
+            },
+            "withInspectorControls"
+          );
+
+          return withInspectorControls(BlockEdit)(props);
+        } else {
+          return wp.element.createElement(BlockEdit, props);
+        }
+      };
     }
   );
 });
